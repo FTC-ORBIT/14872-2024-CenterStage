e8a0e9372662983e782ea400bd3b209efc10312d
Birdoflames <drnmalka@gmail.com>
2023-11-13 15:15:37 +0200

    Added Deplete subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/deplete/DepleteConstants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robotSubSystems.deplete;
+
+public class DepleteConstants {
+    public static final float rightOpenPos = 100;
+    public static final float rightClosedPos = 0;
+    public static final float leftOpenPos = 100;
+    public static final float leftClosedPos = 0;
+
+}
