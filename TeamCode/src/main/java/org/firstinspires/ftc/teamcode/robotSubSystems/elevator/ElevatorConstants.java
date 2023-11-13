b466607da5c45112caa53ca6ef885a29bc492e1c
IdoDePro <idozipori08@gmail.com>
2023-11-13 15:15:53 +0200

    add Elevator code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/elevator/ElevatorConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/elevator/ElevatorConstants.java
@@ -4,7 +4,15 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ElevatorConstants {
-    public static final float groundHeight = -100;
+    public static final float lowHeight = 0;
+    public static final float intakeHeight = 0;
+    public static final float midHeight = 0;
+    public static final float kp = 0;
+    public static final float ki = 0;
+    public static final float kd = 0;
+    public static final float kf = 0;
+    public static final float izone = 0;
+/*    public static final float groundHeight = -100;
     public static final float intakeHeight = groundHeight;
     public static final float lowHeight = 15200;
     public static final float midHeight = 23885;
@@ -23,6 +31,7 @@ public class ElevatorConstants {
     //dani yalechan
 //6339
     //2000
+     */
 
 }
 
