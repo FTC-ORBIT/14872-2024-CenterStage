d423cde0688b6fa3e44f8ce2c43fc5aedadd2b19
legendsobosob <ng7102008@big.atid.org.il>
2024-02-28 18:47:00 +0200

    should slow down the robot while the robot in stats: MIN , LOW and MID. change the outtake open pos so the pixels don't piled on top of each other

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/outtake/OuttakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/outtake/OuttakeConstants.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.robotSubSystems.outtake;
 
 public class OuttakeConstants {
-    public static final float openPos = 0.7f;
+    public static final float openPos = 0.72f;
     public static final float closedPos = 0.78f;
     public static final float outPos = 0.05f;
 }
