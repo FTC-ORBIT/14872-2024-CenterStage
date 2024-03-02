1246a8b8fc53a707bd85d254a3ae09fa0786402d
water221c <71347711+water221c@users.noreply.github.com>
2024-03-03 00:22:35 +0200

    New test() method to use Gamepad for setting Rectangles coordinates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/camera/PropThreshold.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/camera/PropThreshold.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.robotSubSystems.camera;
 
 import android.graphics.Canvas;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
@@ -15,9 +17,9 @@ import org.opencv.imgproc.Imgproc;
 
 public class PropThreshold implements VisionProcessor {
     Mat testMat = new Mat();
-        Mat highMat = new Mat();
+    Mat highMat = new Mat();
     Mat lowMat = new Mat();
-        Mat finalMat = new Mat();
+    Mat finalMat = new Mat();
     double Threshold = 0.015;
 
     public static String OutStr = "none"; //Set a default value in case vision does not work
@@ -74,6 +76,7 @@ public class PropThreshold implements VisionProcessor {
     public void init(int width, int height, CameraCalibration calibration) {
 
     }
+
     public void initProp() {
 
     }
@@ -95,26 +98,23 @@ public class PropThreshold implements VisionProcessor {
         Scalar highHSVRedLower = new Scalar(160, 100, 20); //Wraps around Color Wheel
         Scalar highHSVRedUpper = new Scalar(180, 255, 255);
 
-        Scalar HSVYellowLower = new Scalar(10, 60, 20);
-        Scalar HSVYellowHigher = new Scalar(40, 255, 255);
-
-        if(PropColor == PropColorEnum.BLUE) {
+        if (PropColor == PropColorEnum.BLUE) {
             Core.inRange(testMat, lowHSVBlueLower, lowHSVBlueUpper, finalMat);
-        } else{
+        } else {
             Core.inRange(testMat, lowHSVRedLower, lowHSVRedUpper, lowMat);
             Core.inRange(testMat, highHSVRedLower, highHSVRedUpper, highMat);
-            Core.bitwise_or(lowMat,highMat, finalMat);
+            Core.bitwise_or(lowMat, highMat, finalMat);
         }
 //        Core.inRange(testMat, lowHSVBlueLower, lowHSVBlueUpper,lowMat);
 
         testMat.release();
 
-         lowMat.release();
-         highMat.release();
+        lowMat.release();
+        highMat.release();
 
-         leftBox = Core.sumElems(finalMat.submat(activeLeftRect)).val[0];
-         middleBox = Core.sumElems(finalMat.submat(activeMiddleRect)).val[0];
-         rightBox = Core.sumElems(finalMat.submat(activeRightRect)).val[0];
+        leftBox = Core.sumElems(finalMat.submat(activeLeftRect)).val[0];
+        middleBox = Core.sumElems(finalMat.submat(activeMiddleRect)).val[0];
+        rightBox = Core.sumElems(finalMat.submat(activeRightRect)).val[0];
 
 //        blueLeftBoxFar = Core.sumElems(finalMat.submat(LEFT_RECTANGLE_FAR)).val[0];
 //        blueMiddleBoxFar = Core.sumElems(finalMat.submat(MIDDLE_RECTANGLE_FAR)).val[0];
@@ -131,13 +131,13 @@ public class PropThreshold implements VisionProcessor {
 //        averagedBlueRightBoxFar = blueRightBoxFar / RIGHT_RECTANGLE_FAR.area() / 255;
 
 
-        if(averagedLeftBox > Threshold && averagedLeftBox > averagedMiddleBox){        //Must Tune Red Threshold
+        if (averagedLeftBox > Threshold && averagedLeftBox > averagedMiddleBox) {        //Must Tune Red Threshold
             OutStr = "Left";
             PropPos = PropPosEnum.LEFT;
-        }else if(averagedMiddleBox > Threshold && averagedRightBox < averagedMiddleBox){
+        } else if (averagedMiddleBox > Threshold && averagedRightBox < averagedMiddleBox) {
             OutStr = "Center";
             PropPos = PropPosEnum.CENTER;
-        }else if(averagedRightBox > Threshold){
+        } else if (averagedRightBox > Threshold) {
             OutStr = "Right";
             PropPos = PropPosEnum.RIGHT;
         }
@@ -166,12 +166,10 @@ public class PropThreshold implements VisionProcessor {
                 new Scalar(0, 255, 0), 10);
 
 
-   //     lowMat.copyTo(frame); /*This line should only be added in when you want to see your custom pipeline
+//     lowMat.copyTo(frame); /*This line should only be added in when you want to see your custom pipeline
 //                                  on the driver station stream, do not use this permanently in your code as
 //                                  you use the "frame" mat for all of your pipelines, such as April Tags*/
         return frame;            //You do not return the original mat anymore, instead return null
-
-
 
 
     }
@@ -182,11 +180,119 @@ public class PropThreshold implements VisionProcessor {
 
     }
 
-    public String GetPropPosition(){
+    public String GetPropPosition() {
         return OutStr;
     }
 
-    public PropPosEnum EnumGetPropPos(){
+    public PropPosEnum EnumGetPropPos() {
         return PropPos;
     }
+
+
+//    public boolean lastLeft = false;
+//    public boolean lastRight = false;
+//    public boolean lastRT = false;
+//    public boolean lastLT = false;
+
+    public Rect rectHitL = new Rect(20, 20, 100, 100);
+    public Rect rectHitR = new Rect(20, 20, 100, 100);
+    public Rect rectMissL = new Rect(20, 20, 100, 100);
+    public Rect rectMissR = new Rect(20, 20, 100, 100);
+
+    public Rect activeRect = null;
+    public int activeRectIndx = 0;
+    public String activeRectStr = "rectHitL";
+    public int rectStep = 5;
+    public Gamepad lastGamepad = new Gamepad();
+
+    public void test(Gamepad gamepad, Telemetry telemetry) {
+
+        if (activeRect == null) activeRect = activeLeftRect;
+        if (gamepad.share && !lastGamepad.share) {
+            activeRectIndx++;
+            if (activeRectIndx > 3)
+                activeRectIndx = 0;
+
+            switch (activeRectIndx) {
+                case 0:
+                    activeRect = rectHitL;
+                    activeRectStr = "rectHitL";
+                    break;
+                case 1:
+                    activeRect = rectHitR;
+                    activeRectStr = "rectHitR";
+                    break;
+                case 2:
+                    activeRect = rectMissL;
+                    activeRectStr = "rectMissL";
+                    break;
+                case 3:
+                    activeRect = rectMissR;
+                    activeRectStr = "rectMissR";
+                    break;
+            }
+        }
+
+
+        if (gamepad.dpad_left && !lastGamepad.dpad_left) {
+            activeRect.x -= rectStep;
+            if (activeRect.x < 0)
+                activeRect.x = 0;
+        } else if (gamepad.dpad_right && !lastGamepad.dpad_right) {
+            activeRect.x += rectStep;
+            if (activeRect.x > testMat.cols() - 1)
+                activeRect.x = testMat.cols() - 1;
+        }
+        if (gamepad.dpad_down && !lastGamepad.dpad_down) {
+            activeRect.y -= rectStep;
+            if (activeRect.y < 0)
+                activeRect.y = 0;
+        } else if (gamepad.dpad_up && !lastGamepad.dpad_up) {
+            activeRect.y += rectStep;
+            if (activeRect.y > testMat.rows() - 1)
+                activeRect.y = testMat.rows() - 1;
+        }
+
+        if (gamepad.x && !lastGamepad.x) {
+            activeRect.width -= rectStep;
+            if (activeRect.width < 1)
+                activeRect.width = 1;
+        } else if (gamepad.b && !lastGamepad.b) {
+            activeRect.width += rectStep;
+            if (activeRect.br().x > testMat.cols() - 1)
+                activeRect.width = testMat.cols() - activeRect.x;
+        }
+        if (gamepad.a && !lastGamepad.a) {
+            activeRect.height -= rectStep;
+            if (activeRect.height < 0)
+                activeRect.height = 0;
+        } else if (gamepad.y && !lastGamepad.y) {
+            activeRect.height += rectStep;
+            if (activeRect.br().y > testMat.rows() - 1)
+                activeRect.height = testMat.rows() - activeRect.y;
+        }
+
+
+        if (gamepad.left_bumper && !lastGamepad.left_bumper) {
+            rectStep -= 1;
+        } else if (gamepad.right_bumper && !lastGamepad.right_bumper) {
+            rectStep += 1;
+        }
+
+        telemetry.addLine(String.format("Set Rectangle:  %s   - Indx: %d",
+                activeRectStr, activeRectIndx));
+        telemetry.addData("x, y, width, height:  ", activeRect.toString());
+        telemetry.addData("TL = ", activeRect.tl());
+        telemetry.addData("BR = ", activeRect.br());
+        telemetry.addLine("");
+        telemetry.addData("rectStep = ", rectStep);
+
+//        lastLeft = gamepad.left_bumper;
+//        lastRight = gamepad.right_bumper;
+//        lastLT = gamepad.dpad_left;
+//        lastRT = gamepad.dpad_right;
+        lastGamepad.copy(gamepad);
+    }
+
 }
+
